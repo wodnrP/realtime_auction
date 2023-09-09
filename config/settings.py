a0e3964d57d6f712e3a4984ab0b1ef6dbcdf5293
wodnrP <wogajang22@naver.com>
2023-09-09 14:01:00 +0900

    feat: Mongo DB 연결
    NoSQL 개발환경 위해 Mongo DB 연결
    djongo 패키지 추가

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'realtimeAuction_db',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
 
