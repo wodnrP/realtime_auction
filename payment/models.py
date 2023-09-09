627bba860e75da5485b1412f080d12a363b58fdf
ramyo564 <yohan032yohan@gmail.com>
2023-09-09 12:38:51 +0900

    fix: 모델설정 오타 수정
    users -> user.users
    products -> product.products

--- payment/models.py
+++ payment/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from product.models import Products
 
 # Create your models here.
 
 
 class Payments(models.Model):
-    buyer = models.ForeignKey("users", on_delete=models.CASCADE)
-    product_id = models.ForeignKey("products", on_delete=models.CASCADE)
+    buyer = models.ForeignKey("user.users", on_delete=models.CASCADE)
+    product_id = models.ForeignKey("product.products", on_delete=models.CASCADE)
     payment_type = models.CharField(max_length=100)
     payment_date = models.DateTimeField(auto_now_add=True)
     total_price = models.IntegerField()
