6b021079028fea6de7d40d3d49d483365553993a
wodnrP <wogajang22@naver.com>
2023-09-10 19:32:25 +0900

    feat: Wishlist Model 추가

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class WishList(models.Model):
+    users_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    wishlist_active = models.BooleanField(default=False)
+
