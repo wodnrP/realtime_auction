bfbda3ddce72acac51fc9dcf563334124dc73f98
uniqquej <cjy4477@gmail.com>
2023-09-11 12:56:07 +0900

    feat: penalty model 추가

--- penalty/models.py
+++ penalty/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+from user.models import User
+
+class Penalty(models.Model):
+    user_id = models.ForeignKey(User,  on_delete=models.CASCADE)
+    buy_penalty = models.IntegerField()
+    buy_penalty_content = models.CharField(max_length=255)
+    sell_penalty = models.IntegerField()
+    sell_penalty_content = models.CharField(max_length=255)
