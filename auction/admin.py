e2acdb16a9106274b24911b2e541e19c58ae9761
수현 <id_suhyun@naver.com>
2023-09-12 17:17:30 +0900

    feat: auction 모델 추가

--- auction/admin.py
+++ auction/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Auction
 
-# Register your models here.
+
+@admin.register(Auction)
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = (
+        "auction_users",
+        "auction_chat_name",
+        "auction_chat_open_at",
+    )
+
+    list_display_links = (
+        "auction_users",
+        "auction_chat_name",
+    )
+
+    list_filter = ("auction_chat_open_at",)
+
+    search_fields = (
+        "auction_users",
+        "auction_chat_name",
+        "auction_chat_open_at",
+    )
--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from djongo import models
+from django.utils import timezone
+from user.models import User
+from product.models import Products
 
-# Create your models here.
+
+"""
+auction_id : 경매가 진행되는 실시간 채팅방 고유 번호 / pk로 잡혀서 따로 필요 없을듯
+auction_users : 경매 물건을 파는 사람 == 채팅방 주인
+auction_chat_name : 경매 채팅방 이름(경매 제품 이름)
+auction_chat_start_at : 경매 시작 시간
+auction_chat_finish_at : 경매 마감 시간(user가 경매를 종료 했을 경우 마감 시간이 설정 됨)
+"""
+
+
+class Auction(models.Model):
+    # auction_id = models.AutoField(primary_key=True)
+    auction_users = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction_chat_name = models.ForeignKey(Products, on_delete=models.CASCADE)
+    auction_chat_open_at = models.DateTimeField(blank=False)
+    auction_chat_close_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.auction_chat_name
+
+    def close_auction(self):
+        """
+        판매자가 경매를 마감한 시간을 경매 마감시간으로 설정합니다.
+        """
+        self.auction_chat_close_at = timezone.now()
+        self.save()
+
+    class Meta:
+        ordering = "-auction_chat_open_at"
