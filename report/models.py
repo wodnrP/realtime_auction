d5a63840ea26c7d7d0c12deb69e62b10c4058515
ssu-uky <id_suhyun@naver.com>
2023-09-13 07:04:01 +0900

    feat: report 모델 chatting_room, suspect 추가

--- report/models.py
+++ report/models.py
@@ -4,6 +4,8 @@ from djongo import models
 reporter : 신고자
 report_type : 신고 유형 (PROFANITY:욕설, ADVERTISEMENT:광고, SPAM:스팸 및 도배)
 report_at : 신고 시간
+report_chatting_room : 신고된 채팅방 / 경매 채팅방(auction) 참조
+report_suspect : 신고 대상자 (신고당한 사람)
 report_content : 신고 내용 (채팅방에서 불러오기)
 """
 
@@ -27,6 +29,16 @@ class Report(models.Model):
     )
     report_at = models.DateTimeField(
         auto_now_add=True,
+    )
+    report_chatting_room = models.ForeignKey(
+        "auction.Auction",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    report_suspect = models.ForeignKey(
+        "user.User",
+        on_delete=models.CASCADE,
+        related_name="suspects",
     )
     report_content = models.TextField()
 
