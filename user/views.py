ed8f0c0771a1dbb51f7e25cc6571f6f06dba4f40
uniqquej <cjy4477@gmail.com>
2023-09-14 20:59:12 +0900

    fix: 로그인 비밀번호 확인 오류 수정
    user_check 변수 수정 > exist와 first구문으로 나눠줌

--- user/views.py
+++ user/views.py
@@ -60,9 +60,10 @@ class CheckAuthNumberView(APIView):
         phone_number = request.data["phone_number"]
         input_number = request.data["input_number"]
 
-        user_check = User.objects.filter(phone_number=phone_number).exists()
-        if user_check:
-            if user_check.auth_number == input_number:
+        user_check = User.objects.filter(phone_number=phone_number)
+        
+        if user_check.exists():
+            if user_check.first().auth_number == input_number:
                 return Response({"msg": "인증이 완료되었습니다."}, status=status.HTTP_200_OK)
             else:
                 return Response(
@@ -95,22 +96,19 @@ class LoginView(APIView):
         phone_number = request.data["phone_number"]
         password = request.data["password"]
 
-        user_check = (
-            User.objects.filter(phone_number=phone_number).exists()
-        )
+        user_check = User.objects.filter(phone_number=phone_number)
 
-        if not user_check:
+        if not user_check.exists():
             return Response(
                 {"msg": "존재하지 않는 유저입니다."}, status=status.HTTP_400_BAD_REQUEST
             )
 
-        if not check_password(password, user_check.password):
+        if not check_password(password, user_check.first().password):
             return Response(
                 {"msg": "비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST
             )
 
         user = authenticate(phone_number=phone_number, password=password)
-        print(user)
 
         if user.is_authenticated:
             token = MyTokenObtainPairSerializer.get_token(user)
@@ -119,5 +117,5 @@ class LoginView(APIView):
 
             return Response(
                 {"msg": "로그인 성공", "access": access_token, "refresh": refresh_token},
-                status=status.HTTP_200_OK,
+                status=status.HTTP_200_OK
             )
