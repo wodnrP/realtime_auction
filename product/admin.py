caed8f727b9e1eca22cec875f87bce8603ae6401
ramyo564 <yohan032yohan@gmail.com>
2023-09-14 13:38:33 +0900

    chore: 어드민 패널 수정

--- product/admin.py
+++ product/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
+from django import forms
 from .models import Products, ProductImages, Categories, CategoryItem
 
 
+class ProductsAdminForm(forms.ModelForm):
+    class Meta:
+        model = Products
+        exclude = ("auction_start_at", "auction_end_at")
+
+
 class ProductsAdmin(admin.ModelAdmin):
+    form = ProductsAdminForm
     list_display = (
         "product_name",
         "seller_id",
         "product_price",
         "auction_start_at",
         "auction_end_at",
-        "auction_state",
+        "auction_active",
     )
-    list_filter = ("seller_id", "auction_state")
+    list_filter = ("seller_id", "auction_active")
     search_fields = ("product_name", "seller_id__username")
 
 
