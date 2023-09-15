d7f078cda7ad86c2992e33ba61fdbf1d44ba0b7c
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 02:46:57 +0900

    feat: jwt 인증 및 권한 검사 추가

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,6 @@ from product import views
 
 urlpatterns = [
     path("all-products", views.ProductsView.as_view(), name="all-products"),
-    path("new-product", views.NewProductView.as_view(), name="all-products"),
+    path("new-product", views.NewProductView.as_view(), name="new-product"),
+    path("<str:pk>/delete", views.DeleteProductView.as_view(), name='delete-product'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,11 +1,13 @@
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from .models import Products
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProductsSerializer
 from rest_framework import status
 from django.utils import timezone
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 now = timezone.now()
 
@@ -20,6 +22,8 @@ class ProductsView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 class NewProductView(APIView):
     def post(self, request):
         # POST 요청 처리 로직
@@ -28,3 +32,12 @@ class NewProductView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+class DeleteProductView(APIView):
+    def delete(self, request, pk):
+        product = get_object_or_404(Products, pk=pk, auction_active=True)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
