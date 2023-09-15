04a53dd94ba4778b13890434a5268b3163b6e49e
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 03:49:20 +0900

    feat: 상품 검색기능 추가
    트라이 자료구조로 구현된 라이브러리.
    앞에 몇 글자만 입력해도 검색 가능

--- product/filters.py
+++ product/filters.py
@@ -3,6 +3,8 @@ from .models import Products
 
 
 class ProductsFilter(filters.FilterSet):
+    keyword = filters.CharFilter(field_name="product_name", lookup_expr="icontains")
+
     class Meta:
         model = Products
-        fields = {"category"}
+        fields = ("category", "keyword")
