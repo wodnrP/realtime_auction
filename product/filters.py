3b0f7ea39e30f1ac048bb409cf0056a678616930
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 18:30:16 +0900

    refactor: 카테고리 필터 id -> name 으로 변경
    카테고리를 걸어서 필터링 할 때 id 값이 아닌 name으로도 조회 가능하게 변경했습니다.

--- product/filters.py
+++ product/filters.py
@@ -4,6 +4,7 @@ from .models import Products
 
 class ProductsFilter(filters.FilterSet):
     keyword = filters.CharFilter(field_name="product_name", lookup_expr="icontains")
+    category = filters.CharFilter(field_name="category__category_name", lookup_expr="icontains")
 
     class Meta:
         model = Products
