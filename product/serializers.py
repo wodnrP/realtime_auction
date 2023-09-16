6b1846a6e64567835d6bd2231bf6aae4b1d13f0f
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 15:54:17 +0900

    refactor: ProductsSerializer 이미지 url 추가

--- product/serializers.py
+++ product/serializers.py
@@ -5,10 +5,23 @@ from .models import Products, ProductImages
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImages
-        fields = '__all__'
+        fields = ("product_images",)
 
 
 class ProductsSerializer(serializers.ModelSerializer):
+    product_images = ImageSerializer(
+        many=True, read_only=True, source="productimages_set"
+    )
+
     class Meta:
         model = Products
         fields = "__all__"
+
+    def to_representation(self, instance):
+
+        data = super().to_representation(instance)
+
+        if data["product_images"]:
+            data["product_images"] = data["product_images"][0]["product_images"]
+
+        return data
