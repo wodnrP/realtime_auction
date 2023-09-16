ddfe2b031d524c7df181c0db9bcadde60069745e
wodnrP <wogajang22@naver.com>
2023-09-16 12:27:40 +0900

    chore: wishlist url name 수정

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,6 @@ from django.conf import settings
 from .views import WishlistView
 
 urlpatterns = [
-    path('', WishlistView.as_view(), name="GetWishList"),
-    path('<int:product_id>', WishlistView.as_view(), name="PostDeleteWishList")    
+    path('', WishlistView.as_view(), name="get_wishlist"),
+    path('<int:product_id>', WishlistView.as_view(), name="post_delete_wishlist")    
 ]
