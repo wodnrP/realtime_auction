bf18ce78fd44e0ad3518db5f33e67f2ce38b02de
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 16:00:53 +0900

    chore: views.py- docstring 추가

--- product/views.py
+++ product/views.py
@@ -16,6 +16,18 @@ now = timezone.now()
 
 
 class ProductsView(APIView):
+    """
+    현재 활성화된 경매 상품의 페이지네이션된 목록을 반환합니다.
+
+    Parameters:
+    - request: HTTP GET 요청 객체
+
+    Returns:
+    - Response: 페이지네이션된 상품 목록을 포함한 JSON 응답
+
+    Errors:
+    - 400 Bad Request: 요청 데이터가 유효하지 않은 경우
+    """
     def get(self, request):
         filter_set = ProductsFilter(
             request.GET,
@@ -23,7 +35,6 @@ class ProductsView(APIView):
                 "auction_end_at"
             ),
         )
-        # 페이지네이션
         paginator = PageNumberPagination()
         paginator.page_size = 2
         queryset = paginator.paginate_queryset(filter_set.qs, request)
@@ -34,8 +45,21 @@ class ProductsView(APIView):
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 class NewProductView(APIView):
+    """
+    새로운 상품을 생성하는 뷰입니다.
+
+    인증과 권한이 필요합니다.
+
+    Parameters:
+    - request: HTTP POST 요청 객체
+
+    Returns:
+    - Response: 생성된 상품 정보 또는 오류 메시지를 포함한 JSON 응답
+
+    Errors:
+    - 400 Bad Request: 요청 데이터가 유효하지 않은 경우
+    """
     def post(self, request):
-        # POST 요청 처리 로직
         serializer = ProductsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -46,6 +70,23 @@ class NewProductView(APIView):
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 class DeleteProductView(APIView):
+    """
+    상품을 삭제하는 뷰입니다.
+
+    인증과 권한이 필요하며 활성화된 경매만 삭제할 수 있습니다.
+
+    Parameters:
+    - request: HTTP DELETE 요청 객체
+    - pk: 삭제할 상품의 기본 키
+
+    Returns:
+    - Response: 성공 또는 실패 메시지를 포함한 JSON 응답
+
+    Errors:
+    - 400 Bad Request: 요청 데이터가 유효하지 않은 경우
+    - 403 Forbidden: 상품을 삭제할 권한이 없는 경우
+    - 404 Not Found: 삭제하려는 상품이 존재하지 않는 경우
+    """
     def delete(self, request, pk):
         product = get_object_or_404(Products, pk=pk, auction_active=True)
         product.delete()
@@ -55,9 +96,35 @@ class DeleteProductView(APIView):
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 class ImageView(APIView):
+    """
+    상품 이미지 업로드와 검색을 다루는 뷰입니다.
+
+    인증과 권한이 필요하며 이미지 업로드와 검색을 지원합니다.
+
+    Parameters:
+    - request: HTTP POST 또는 GET 요청 객체
+
+    Returns:
+    - Response: 업로드된 이미지 정보 또는 검색된 이미지 목록을 포함한 JSON 응답
+
+    Errors:
+    - 400 Bad Request: 요청 데이터가 유효하지 않은 경우
+    """
     parser_classes = (MultiPartParser, FormParser)
 
     def post(self, request, *args, **kwargs):
+        """
+        이미지 업로드를 처리합니다.
+
+        Parameters:
+        - request: 이미지 데이터를 포함한 HTTP POST 요청 객체
+
+        Returns:
+        - Response: 업로드된 이미지 정보 또는 오류 메시지를 포함한 JSON 응답
+
+        Errors:
+        - 400 Bad Request: 요청 데이터가 유효하지 않은 경우
+        """
         serializer = ImageSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -67,6 +134,19 @@ class ImageView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, *args, **kwargs):
+        """
+        상품에 연결된 이미지를 검색합니다.
+
+        Parameters:
+        - request: 상품 ID를 포함한 HTTP GET 요청 객체
+
+        Returns:
+        - Response: 상품과 연결된 이미지 URL 목록을 포함한 JSON 응답
+
+        Errors:
+        - 400 Bad Request: 요청 데이터가 유효하지 않은 경우
+        - 404 Not Found: 검색하려는 상품이 존재하지 않는 경우
+        """
         products_id = self.kwargs.get("products_id")
         images = ProductImages.objects.filter(products_id=products_id)
         serializer = ImageSerializer(images, many=True)
