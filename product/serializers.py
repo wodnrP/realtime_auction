f1d09b8d41173ccf16dc1ace471d63a69c77a2bb
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 18:06:51 +0900

    refactor: Products 시리얼라이저 리팩토링
    카테고리 부분 id 값이 아닌 name 값으로 출력하도록 변경

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Products, ProductImages
+from .models import Products, ProductImages, Categories
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -13,13 +13,18 @@ class ProductsSerializer(serializers.ModelSerializer):
         many=True, read_only=True, source="productimages_set"
     )
 
+    category_name = serializers.CharField(source="category.category_name")
+
     class Meta:
         model = Products
         fields = "__all__"
 
     def to_representation(self, instance):
-
         data = super().to_representation(instance)
+
+        # 'category' 필드 대신 'category_name' 필드 사용
+        data["category"] = data["category_name"]
+        del data["category_name"]
 
         if data["product_images"]:
             data["product_images"] = data["product_images"][0]["product_images"]
