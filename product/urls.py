363bf1dda2dfc45ae4465f1f67caeb3ac88e3b1e
ramyo564 <yohan032yohan@gmail.com>
2023-09-16 15:03:03 +0900

    refactor: url 패턴 추가 및 네이밍 변경

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path("all-products", views.ProductsView.as_view(), name="all-products"),
-    path("new-product", views.NewProductView.as_view(), name="new-product"),
-    path("<str:pk>/delete", views.DeleteProductView.as_view(), name='delete-product'),
+    path("all-products", views.ProductsView.as_view(), name="all_products"),
+    path("new-product", views.NewProductView.as_view(), name="new_product"),
+    path("<str:pk>/delete", views.DeleteProductView.as_view(), name='delete_product'),
+    path('upload-images', views.ImageView.as_view(), name="upload_images"),
+    path('images/<str:products_id>', views.ImageView.as_view(), name='get_images'),
 ]
