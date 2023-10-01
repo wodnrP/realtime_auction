5e90e132853715e21dadfa951301a24a721d5a5c
uniqquej <cjy4477@gmail.com>
2023-10-02 06:55:06 +0900

    chore: celery tasks 수정
    역참조를 통해 auction_room 여부를 주기적으로  확인하여, auction room이 있다면 추가, 없다면 auction_active를 확인

--- auction/tasks.py
+++ auction/tasks.py
@@ -1,9 +1,11 @@
 from celery import shared_task
 from datetime import datetime
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import AuctionRoom
 from product.models import Products
+
 
 
 @shared_task
@@ -12,16 +14,22 @@ def check_and_create_auction_rooms():
     # 현재 시간이 지난 상품들 중 아직 AuctionRoom이 생성되지 않은 것만 선택
     products_to_start = Products.objects.filter(
         auction_start_at__lte=timezone.now(),
-        auction_room__isnull=True,
         product_active=True,
     )
 
     for product in products_to_start:
         # 새로운 AuctionRoom 생성
-        AuctionRoom.objects.create(
-            auction_host=product.seller_id,
-            auction_room_name=product,
-            auction_active=True,  # 경매 활성화 상태로 설정
-        )
+        try:
+            auction_room_check = product.auction_room
+            if auction_room_check.auction_end_at <= timezone.now():
+                auction_room_check.auction_active = False
+                auction_room_check.save()
+            
+        except:
+            AuctionRoom.objects.create(
+                auction_host=product.seller_id,
+                auction_room_name=product,
+                auction_active=True,  # 경매 활성화 상태로 설정
+            )
 
-print("1")
+    return 'check auction'
