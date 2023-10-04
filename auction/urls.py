d44a1b2cc1bcac355b9051fa15af78c9011b0bfe
uniqquej <cjy4477@gmail.com>
2023-10-05 03:38:54 +0900

    chore: 사용하지 않는 auction view 정리

--- auction/urls.py
+++ auction/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from django.conf import settings
-from .views import AuctionListView, UserAuctionListView, NewAuctionRoomView, AuctionMessageView, index, room
 
+from .views import AuctionListView, UserAuctionListView, NewAuctionRoomView
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("<str:room_name>", room, name="room"),
     path("list", AuctionListView.as_view(), name="auction-list"),
     path("list/<int:user_pk>", UserAuctionListView.as_view(), name="user-auction-list"),
-    path("room/new", NewAuctionRoomView.as_view(), name="auction-room"),
-    path("message", AuctionMessageView.as_view(), name="auction-message"),
+    path("room/<int:room_pk>", NewAuctionRoomView.as_view(), name="auction-room"),
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,120 +1,45 @@
 from django.shortcuts import render
-from .serializers import (
-    AuctionRoomSerializer,
-    AuctionRoomListSerializer,
-    AuctionMessageSerializer,
-)
-from .models import AuctionRoom, AuctionMessage
-from product.models import Products
+from .serializers import AuctionRoomSerializer
+
+from .models import AuctionRoom
+
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import NotFound, PermissionDenied
-from django.db.models import Q, F
+from rest_framework.exceptions import PermissionDenied
 
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 
-def index(request):
-    return render(request, "auction/index.html")
-
-def room(request, room_name):
-    return render(request, "auction/room.html", {"room_name": room_name})
-
+ 
 class AuctionListView(APIView):
     def get(self, request):
         rooms = AuctionRoom.objects.all()
-        serializer = AuctionRoomListSerializer(rooms, many=True)
+        serializer = AuctionRoomSerializer(rooms, many=True)
         return Response(serializer.data)
 
 
 class UserAuctionListView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user_pk = request.user.pk
-        queryset = (
-            Products.objects.filter(status=False, auction_winner__isnull=False)
-            .filter(Q(auction_host=user_pk) | Q(auction_winner=user_pk))
-            .annotate(updated_at=F("auction_room__updated_at"))
-            .order_by("-updated_at")
-            .select_related(
-                "auction_room",
-                "auction_room__auction_host",
-                "auction_room__auction_winner",
-                "auction_room__auction_final_price",
-            )
-            .prefetch_related("auction_room__auction_paticipants")
-        )
-        serializer = AuctionRoomListSerializer(
-            queryset, many=True, context={"request": request}
-        )
-        return Response(serializer.data)
+    # 경매 주최자 or 경매 낙찰자가 해당 유저인 경매 목록
+    def get(self, request, user_pk):
+        auction_rooms = AuctionRoom.objects.filter(Q(auction_host=user_pk) | Q(auction_winner=user_pk))
+        serializer = AuctionRoomSerializer(auction_rooms, many=True)
+        return Response(serializer.data,  status=status.HTTP_200_OK)
 
 
 class NewAuctionRoomView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def get_object(self, pk):
-        try:
-            return AuctionRoom.objects.get(pk=pk)
-        except AuctionRoom.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, pk):
-        room = self.get_object(pk)
+    #경매 입장시 마감시간, 최고 제시 가격 조회 역할
+    def get(self, request, room_pk):
+        room = get_object_or_404(AuctionRoom, pk=room_pk)
         serializer = AuctionRoomSerializer(room)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = AuctionRoomSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        room = self.get_object(pk)
+    def delete(self, request, room_pk):
+        room = get_object_or_404(AuctionRoom, pk=room_pk)
         if room.auction_host != request.user:
             raise PermissionDenied
         room.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class AuctionMessageView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self, pk):
-        try:
-            return AuctionRoom.objects.get(pk=pk)
-        except AuctionRoom.DoesNotExist:
-            raise NotFound
-
-    def post(self, request):
-        room_id = request.data.get("auction_room")
-        bid_price = request.data.get("auction_bid_price")
-
-        if not room_id or not bid_price:
-            return Response(
-                {"detail": "유효하지 않은 데이터 입니다."}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-        room = AuctionRoom.objects.get(id=room_id)
-
-        starting_price = room.starting_price
-        last_bid_price = room.auction_final_price or starting_price
-
-        if bid_price <= last_bid_price:
-            return Response(
-                {"detail": "입력 가격은 이전 가격보다 높아야합니다."}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-        serializer = AuctionMessageSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            room.auction_final_price = bid_price
-            room.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
