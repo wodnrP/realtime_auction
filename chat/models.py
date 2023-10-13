27c031e104384266f2e840e578e17c7ee0232970
wodnrP <wogajang22@naver.com>
2023-10-13 17:23:48 +0900

    chore: chat models.py 수정
    1:1 메세지의 전송 시간을 조회하기 위해 message_time 필드 추가
    message_time 기준 정렬
    필드별 이해를 돕기 위해 verbose_name 속성 추가

--- chat/models.py
+++ chat/models.py
@@ -3,10 +3,36 @@ from user.models import User
 from auction.models import AuctionRoom
 # Create your models here.
 class Chatting(models.Model):
-    auction_id=models.ForeignKey(AuctionRoom,on_delete=models.CASCADE)
+    auction_id=models.OneToOneField(
+        AuctionRoom,
+        on_delete=models.CASCADE,
+        verbose_name="연결된 경매"
+    )
+    
 
 class Message(models.Model):
-    chatting_id=models.ForeignKey(Chatting, on_delete=models.CASCADE)
-    sender_id=models.ForeignKey(User,on_delete=models.CASCADE)
-    message_type=models.TextField()
-    message_content=models.TextField()
+    chatting_id=models.ForeignKey(
+        Chatting, 
+        on_delete=models.CASCADE,
+        verbose_name="1:1 채팅방",
+    )
+    sender_id=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="메세지 발신자",
+    )
+    message_type=models.TextField(
+        max_length=100,
+        verbose_name="메세지 유형",
+    )
+    message_content=models.TextField(
+        max_length=200,
+        verbose_name="메세지 내용",
+    )
+    message_time=models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="전송 시간",
+    )
+
+    class Meta:
+        ordering = ["-message_time"]
