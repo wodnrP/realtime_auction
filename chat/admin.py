0958c43817d04c5d2c6714bf3ca2a24759aea231
wodnrP <wogajang22@naver.com>
2023-10-17 22:46:19 +0900

    feat: chat admin page 추가

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
-
+from .models import Chatting, Message
 # Register your models here.
+
+@admin.register(Chatting)
+class ChattingAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "auction_id",
+    )
+
+    list_display_links = (
+        "pk",
+        "auction_id",
+    )
+
+    search_fields = (
+        "auction_id"
+    )
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "chatting_id",
+        "sender_id",
+        "message_type",
+        "message_content",
+        "message_time",
+    )
+
+    list_display_links = (
+        "pk",
+        "chatting_id",
+        "sender_id",
+        "message_type",
+        "message_content",
+        "message_time",
+    )
