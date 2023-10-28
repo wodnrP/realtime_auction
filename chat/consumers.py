471640d6236cee80d156eb4d8412fba18df137d5
wodnrP <wogajang22@naver.com>
2023-10-28 17:20:48 +0900

    fix: chat/consumers.py 수정
    1:1 메시지가 저장되도록 create_or_update_chat_message 함수 사용
    첫 공지 메세지와 사용자 채팅 함수 send, chat_message로 분리

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,20 +8,13 @@ from .models import Chatting, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["auction_pk"]
+        self.room_name = self.scope["url_route"]["kwargs"]["chatting_pk"]
         self.room_group_name = f"chat_{self.room_name}"
         self.user = self.scope["user"]
 
         # 로그인 된 사용자만 채팅 참여
         if not self.user.is_authenticated:
             await self.close()
-
-        host = await self.get_chat_room()
-
-        if not host:
-            await self.close()
-        else:
-            self.host = host
 
         # 채팅방 그룹에 추가
         await self.channel_layer.group_add(
@@ -31,35 +24,56 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
         await self.chat_message("경매가 완료되어 1:1 채팅이 시작되었습니다.")
-        await self.enter_or_exit_room("enter")
 
     async def disconnect(self, close_code):
-        await self.enter_or_exit_room("exit")
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        await self.chat_message(message)
+        await self.send_message(message)
+        await self.crete_or_update_chat_message(
+            self.user, self.room_name, message
+        )
 
+    # 1:1 채팅 메세지
+    async def send_message(self, message):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            },
+        )
+
+    # 채팅 시작 메세지 
     async def chat_message(self, event):
-        message = event["message"]
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(
+            text_data=json.dumps(
+                {   
+                    "type": "send_message",
+                    "message": event,
+                }
+            )
+        )
 
+    # 채팅 내용 저장
     @database_sync_to_async
-    def crete_or_update_chat_message(self, user, room_name):
+    def crete_or_update_chat_message(self, user, room_name, message):
         try:
             chat_room = Chatting.objects.filter(pk=room_name)
             if chat_room.exists():
                 chat_message = Message.objects.filter(
                     chatting_id=chat_room[0], 
                     sender_id=user,
+                    message_content=message,
                 )
 
                 if not chat_message.exists():
                     new_chat_message = Message.objects.create(
                         chatting_id=chat_room[0],
                         sender_id=user,
+                        message_content=message,
                     )
                     new_chat_message.save()
                     return True
