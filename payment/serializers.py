f399e4117812f20d607ec7a87d4c666f4e9d895d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2023-10-31 04:47:40 +0900

    chore: 카카오페이Ready시리얼라이저 작성

--- payment/serializers.py
+++ payment/serializers.py
@@ -14,3 +14,15 @@ class WinningBidListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
         fields = "__all__"
+
+class KakaoPayReadySerializer(serializers.Serializer):
+    buyer = serializers.CharField(source="buyer.username", read_only=True)
+    product_name = serializers.CharField(
+        source="product_name.product_name", read_only=True
+    )
+    total_price = serializers.CharField(
+        source="total_price.auction_final_price", read_only=True
+    )
+    url = serializers.URLField(
+        source="kakao_pay_url" , read_only=True
+    )
