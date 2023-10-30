52e6468f6b184fda536e9a62d971c3ec70972801
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2023-10-30 18:54:32 +0900

    chore: Payments 모델 필드명 수정
    product_id -> product_name
    total_price -> AuctionRoom에서 최종가격 갖고오기
    paid -> 결제 여부

--- payment/models.py
+++ payment/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from user.models import User
 from product.models import Products
+from auction.models import AuctionRoom
 
 # Create your models here.
 
 
 class Payments(models.Model):
     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
+    product_name = models.ForeignKey(Products, on_delete=models.CASCADE)
     payment_type = models.CharField(max_length=100)
     payment_date = models.DateTimeField(auto_now_add=True)
-    total_price = models.IntegerField()
+    total_price = models.ForeignKey(AuctionRoom, on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "Payment"
         verbose_name_plural = "Payments"
+
+    def __str__(self):
+        return f"구매자 : {self.buyer}, 낙찰아이템 : {self.product_name}"
