32a285d2d4e6f013cf1cdba8351f00d6008230fa
uniqquej <cjy4477@gmail.com>
2023-11-04 06:49:47 +0900

    style: black formatting

--- penalty/models.py
+++ penalty/models.py
@@ -18,18 +18,21 @@ class Penalty(models.Model):
     @receiver(post_save, sender="penalty.Penalty")
     def update_user_access(sender, instance, created, **kwargs):
         if created:
-
             user = User.objects.get(id=instance.user_id.id)
-            buy_penalty = Penalty.objects.filter(user_id=instance.user_id.id, penalty_type='buy').count()
-            sell_penalty = Penalty.objects.filter(user_id=instance.user_id.id, penalty_type='sell').count()
+            buy_penalty = Penalty.objects.filter(
+                user_id=instance.user_id.id, penalty_type="buy"
+            ).count()
+            sell_penalty = Penalty.objects.filter(
+                user_id=instance.user_id.id, penalty_type="sell"
+            ).count()
             # user.can_buy가 true이고, 패널티의 개수가 3개 이상일 때 구매 불가 상태로 변경
-            if instance.penalty_type == 'buy':
+            if instance.penalty_type == "buy":
                 if user.can_buy & (buy_penalty >= 3):
                     user.can_buy = False
-            
+
             # user.can_sell가 true이고, 패널티의 개수가 3개 이상일 때 판매 불가 상태로 변경
-            elif instance.penalty_type == 'sell':
+            elif instance.penalty_type == "sell":
                 if user.can_sell & (sell_penalty >= 3):
                     user.can_sell = False
-            
+
             user.save()
